125b0570d7f19cffc07518b87a067c7a571a920b
Florencia Zabala <zabalafaz@gmail.com>
2021-11-12 10:34:09 -0300

    Se agregan cambios en página de error

--- src/main/java/com/egg/library/exeptions/ApiExceptionHandler.java
+++ src/main/java/com/egg/library/exeptions/ApiExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.egg.library.exeptions;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,9 +16,15 @@ public class ApiExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFoundException.class})
-    @ResponseBody
-    public ErrorMessage notFoundRequest(HttpServletRequest request,Exception exception){
-        return new ErrorMessage(exception,request.getRequestURI());
+   // @ResponseBody
+    public ModelAndView notFoundRequest(HttpServletRequest request, NoHandlerFoundException exception){
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("codeError","404");
+        model.addObject("textError","Not found");
+        model.addObject("description",""+exception.getMessage());
+
+        return model;
+        //return new ErrorMessage(exception,request.getRequestURI());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
